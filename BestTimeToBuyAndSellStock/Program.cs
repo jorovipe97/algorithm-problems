b445c9c662b7120df4820bf037731e689a135279
Jose Villalobos <jorovipe@gmail.com>
2021-04-30 11:43:39 -0500

    Buy sell stock try 1

---
+++ BestTimeToBuyAndSellStock/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BestTimeToBuyAndSellStock
+{
+    /// <summary>
+    /// https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+    /// You are given an array prices where prices[i] is the price of a given stock on the ith day.
+    /// You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+    /// Return the maximum profit you can achieve from this transaction.If you cannot achieve any profit, return 0.
+    /// </summary>
+    public class Solution
+    {
+        public int MaxProfit(int[] prices)
+        {
+            // [7,1,5,3,6,4]
+            // 1. from left to right which is the lowest number.
+            // 2. save position of lowest number.
+            // 3. from right to left which is the biggest number.
+
+            int lowestIndex = -1;
+            int lowestValue = int.MaxValue;
+            // Find from left to right which is the lowest number.
+            for (int i = 0; i < prices.Length; i++)
+            {
+                int todayPrice = prices[i];
+
+                if (todayPrice < lowestValue)
+                {
+                    lowestIndex = i;
+                    lowestValue = todayPrice;
+                }
+            }
+
+            // If the lowest is en the tail of the array.
+            // then we dont have a possible solution.
+            if (lowestIndex == prices.Length - 1)
+            {
+                return 0;
+            }
+
+            int biggestIndex = -1;
+            int biggestValue = -1;
+            // find from right to left which is the biggest number.
+            for (int i = prices.Length - 1; i > lowestIndex; i--)
+            {
+                int todayPrice = prices[i];
+
+                if (todayPrice > lowestValue && todayPrice > biggestValue)
+                {
+                    biggestIndex = i;
+                    biggestValue = todayPrice;
+                }
+            }
+
+            if (biggestValue == -1)
+            {
+                return 0;
+            }
+
+            return biggestValue - lowestValue;
+        }
+    }
+
+    class Program
+    {
+        /// <summary>
+        /// https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            // expected 5
+            //int[] prices = new int[] { 7, 1, 5, 3, 6, 4 };
+
+            // expected 0
+            //int[] prices = new int[] { 7, 6, 4, 3, 1 };
+
+            // expected 2
+            int[] prices = new int[] { 2, 4, 1 };
+
+
+            var stock = new Solution();
+            int result = stock.MaxProfit(prices);
+            Console.WriteLine($"Result: {result}");
+            Console.Read();
+
+        }
+    }
+}
---
+++ TwoSum-II/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoSum_II
+{
+    class Program
+    {
+        /// <summary>
+        /// Given an array of integers numbers that is already sorted in ascending order, find two numbers such that they add up to a specific target number.
+        /// Return the indices of the two numbers(1-indexed) as an integer array answer of size 2, where 1 <= answer[0] < answer[1] <= numbers.length.
+        /// You may assume that each input would have exactly one solution and you may not use the same element twice.
+        /// https://leetcode.com/problems/two-sum-ii-input-array-is-sorted
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            // https://leetcode.com/problems/two-sum/
+            Console.WriteLine("Two Sums...");
+            int[] nums = new int[] { 2, 7, 11, 15 };
+            int target = 9;
+            int[] result1 = TwoSum(nums, target);
+            int[] result2 = TwoSumTwoPointer(nums, target);
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Brute force solution.
+        /// O(n^2)
+        /// 
+        /// This solution is not considered correct for this problem, since
+        /// it's too slow compared to solutions by other people on leetcode.
+        /// </summary>
+        /// <param name="numbers"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        static public int[] TwoSum(int[] numbers, int target)
+        {
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    int sum = numbers[i] + numbers[j];
+
+                    if (sum == target)
+                    {
+                        return new int[] { i + 1, j + 1 };
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Two pointer solution.
+        /// O(n), or even O(1) in the best case.
+        /// </summary>
+        /// <param name="numbers"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        static public int[] TwoSumTwoPointer(int[] numbers, int target)
+        {
+            int l = 0;
+            int r = numbers.Length - 1;
+
+            // Are we lucky?
+            int sum = numbers[l] + numbers[r];
+            if (sum == target)
+            {
+                return new int[] { l + 1, r + 1 };
+            }
+
+            while (sum != target && l < r)
+            {
+                sum = numbers[l] + numbers[r];
+
+                if (sum > target)
+                {
+                    // This decreases the sum, because the array is sorted.
+                    r--;
+                }
+                else if (sum < target)
+                {
+                    // This increases the sum, because the array is sorted.
+                    l++;
+                }
+                else
+                {
+                    // If reach here, then, the sum is equals to the target.
+                    return new int[] { l + 1, r + 1 };
+                }
+            }
+
+            return null;
+        }
+
+
+    }
+}
