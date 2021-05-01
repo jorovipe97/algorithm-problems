1ed6415b3119ec3b6f4e14965b2373c55b539e35
Jose Villalobos <jorovipe@gmail.com>
2021-05-01 12:08:15 -0500

    Fiished the first two bestimetobuyandsell stock problems

--- BestTimeToBuyAndSellStock/Program.cs
+++ BestTimeToBuyAndSellStock/Program.cs
@@ -65,6 +65,13 @@ namespace BestTimeToBuyAndSellStock
             return maxProfit;
         }
 
+        /// <summary>
+        /// This solution doesn't work for all cases.
+        /// See case where prices is:
+        /// int[] prices = new int[] { 3, 3, 5, 0, 0, 3, 1, 4 };
+        /// </summary>
+        /// <param name="prices"></param>
+        /// <returns></returns>
         public int MaxProfitV2(int[] prices)
         {
             // [7, 1, 5, 3, 6, 4]
@@ -76,41 +83,149 @@ namespace BestTimeToBuyAndSellStock
             // 2. Iterate all sell elements looking and save the one which returns the max profit.
             // 3. Fix the sell element which gave the max profit.
             // 4. Iterate all buy elements until the position of the sell element.
+            if (prices.Length <= 1)
+            {
+                return 0;
+            }
 
             int buyPrice = prices[0];
             int maxProfit = int.MinValue;
-            int sellPriceIndex = -1;
+            // map<profit, sell indexes>
+            // Can happen that multiple sell days has the same profit.
+            var sellPriceIndexes = new Dictionary<int, List<int>>();
             // Iterate all sell elements looking and save the one which returns the max profit.
-            for (int i = 0; i < prices.Length; i++)
+            for (int i = 1; i < prices.Length; i++)
             {
                 int _sellPrice = prices[i];
 
                 int profit = _sellPrice - buyPrice;
-                if (profit > maxProfit)
+                if (profit >= maxProfit)
                 {
+                    // Add profit to map.
+                    if (!sellPriceIndexes.ContainsKey(profit))
+                    {
+                        sellPriceIndexes.Add(profit, new List<int>() { i } );
+                    }
+                    else
+                    {
+                        sellPriceIndexes[profit].Add(i);
+                    }
+
                     maxProfit = profit;
-                    sellPriceIndex = i;
                 }
             }
-            // It's sure sellPriceIndex will always have an valid value.
-
+            
+            // Iterate all the sell prices that generated the max profit.
+            var sellPricesMaxProfit = sellPriceIndexes[maxProfit];
             maxProfit = int.MinValue;
-            int sellPrice = prices[sellPriceIndex];
-            // Iterate all buy elements until the sellPriceIndex
-            for (int i = 0; i < sellPriceIndex; i++)
+            for (int i = 0; i < sellPricesMaxProfit.Count; i++)
             {
-                int _buyPrice = prices[i];
-                int profit = sellPrice - _buyPrice;
-                if (profit > maxProfit)
+                int sellPriceIndex = sellPricesMaxProfit[i];
+                int sellPrice = prices[sellPriceIndex];
+
+                // Iterate all buy elements until the sellPriceIndex
+                for (int j = 0; j < sellPriceIndex; j++)
                 {
-                    maxProfit = profit;
+                    int _buyPrice = prices[j];
+                    int profit = sellPrice - _buyPrice;
+                    if (profit > maxProfit)
+                    {
+                        maxProfit = profit;
+                    }
                 }
             }
 
             // If max profit wasn't initialized, return 0
-            if (maxProfit == int.MinValue)
+            if (maxProfit <= 0)
             {
                 return 0;
+            }
+
+            return maxProfit;
+        }
+
+        public int MaxProfitV3(int[] prices)
+        {
+            // [7, 1, 5, 3, 6, 4]
+            // [7, 6, 4, 3, 1]
+            // [2, 4, 1]
+            // [3, 4, 2, 1]
+            // [7, 5, 1, 3, 6, 4]
+            // 1. Save all items on a map<profit>.
+            // 2. 
+
+            // the max profit will be obtained by getting the min number
+            // that has the max diff ahead.
+
+            // 1. Double pointer, at begining and end of array.
+            // 2. Compute profit.
+            // 3. if current profit is negative move right pointer one position to right.
+            // 4. if current profit is positive move right pointer one position to right.
+
+            // Optimization, if computed for a previously computed buyPrice
+            // then continue to next buyPrice.
+
+            // The value of this map is not used for nothing.
+            var computedBuyPrices = new Dictionary<int, int>();
+
+            int maxProfit = -1;
+            // Fix first element (buy stock).
+            for (int i = 0; i < prices.Length - 1; i++)
+            {
+                int buyPrice = prices[i];
+
+                if (!computedBuyPrices.ContainsKey(buyPrice))
+                {
+                    computedBuyPrices.Add(buyPrice, 0);
+                }
+                else
+                {
+                    // Already computed for current buy price.
+                    // move to next.
+                    continue;
+                }
+
+                for (int j = i + 1; j < prices.Length; j++)
+                {
+                    int sellPrice = prices[j];
+                    int profit = sellPrice - buyPrice;
+                    // compare with remaining elements if there is a number greater
+                    if (profit > maxProfit)
+                    {
+                        maxProfit = profit;
+                    }
+                }
+            }
+
+            //Console.WriteLine($"count: {count}");
+
+            if (maxProfit <= 0)
+            {
+                return 0;
+            }
+
+            return maxProfit;
+        }
+
+        /// <summary>
+        /// After looking on a O(n) solution.
+        /// </summary>
+        /// <param name="prices"></param>
+        /// <returns></returns>
+        public int MaxProfitV4(int[] prices)
+        {
+            int min = int.MaxValue;
+            int maxProfit = 0;
+            for (int i = 0; i < prices.Length; i++)
+            {
+                if (prices[i] < min)
+                {
+                    min = prices[i];
+                }
+                else if (prices[i] - min > maxProfit)
+                {
+                    maxProfit = prices[i] - min;
+                }
             }
 
             return maxProfit;
@@ -136,10 +251,19 @@ namespace BestTimeToBuyAndSellStock
 
             // int[45156] Time Limit Exceeded: 5000ms.
 
+            // expected 0
+            //int[] prices = new int[] { 1 };
+
+            // expected 2
+            //int[] prices = new int[] { 2, 1, 2, 1, 0, 1, 2 };
+
+            // expected 4
+            int[] prices = new int[] { 3, 3, 5, 0, 0, 3, 1, 4 };
+
 
             var stock = new Solution();
             //int result = stock.MaxProfit(prices);
-            int result = stock.MaxProfitV2(prices);
+            int result = stock.MaxProfitV3(prices);
             Console.WriteLine($"Result: {result}");
             Console.Read();
 
---
+++ BestTimeToBuyAndSellStockII/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BestTimeToBuyAndSellStockII
+{
+
+    /// <summary>
+    /// Explanation of thecniques
+    /// https://medium.com/@punitkmr/best-time-to-buy-and-sell-stock-ii-7826845144eb
+    /// </summary>
+    public class Solution
+    {
+        public int MaxProfit(int[] prices)
+        {
+            // The min is like the buy price.
+            int min = int.MaxValue;
+            int totalOperationProfit = 0;
+            int lastOperationProfit = 0;
+            for (int i = 0; i < prices.Length; i++)
+            {
+
+                // Do we found a new min?
+                if (prices[i] < min)
+                {
+                    // and are we still on an operation?
+                    if (lastOperationProfit > 0)
+                    {
+                        // Reset sell variables.
+                        lastOperationProfit = 0;
+                    }
+
+                    min = prices[i];
+                }
+                // Is the operation profit bigger than last profit?
+                else if (prices[i] - min > lastOperationProfit)
+                {
+                    // Save profits.
+                    // Remove profits generated if we would have sold on previous day.
+                    totalOperationProfit -= lastOperationProfit;
+                    lastOperationProfit = prices[i] - min;
+                    // Add profits generated if we would have sold today.
+                    totalOperationProfit += lastOperationProfit;
+                }
+                // if this profit is not bigger than the one in last operation.
+                // sell, and start buying again.
+                else
+                {
+                    // Reset buy/sell variables.
+                    lastOperationProfit = 0;
+                    min = prices[i];
+                }
+
+            }
+
+            return totalOperationProfit;
+        }
+
+
+
+
+        public int MaxProfitV2(int[] prices)
+        {
+
+            int maxProfit = 0;
+            for (int i = 1; i < prices.Length; i++)
+            {
+                if (prices[i] > prices[i - 1])
+                    maxProfit += prices[i] - prices[i - 1];
+            }
+
+            return maxProfit;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+            // Then buy on day 4(price = 3) and sell on day 5(price = 6), profit = 6 - 3 = 3.
+            // expected 7
+            //int[] prices = new int[] { 7, 1, 5, 3, 6, 4 };
+
+            // expected 4
+            //int[] prices = new int[] { 1, 2, 3, 4, 5 };
+
+            // expected 0
+            //int[] prices = new int[] { 7, 6, 4, 3, 1 };
+
+            // expected 7
+            //int[] prices = new int[] { 6, 1, 3, 2, 4, 7 };
+
+            // expected 2
+            //int[] prices = new int[] { 2, 1, 2, 0, 1 };
+
+
+
+            var stock = new Solution();
+            //int result = stock.MaxProfit(prices);
+            int result = stock.MaxProfit(prices);
+            Console.WriteLine($"Result: {result}");
+            Console.Read();
+        }
+    }
+}
